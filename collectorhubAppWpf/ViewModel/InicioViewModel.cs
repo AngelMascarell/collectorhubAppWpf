9e43ad530b4d7e1c4c227cbb89e3a0cb18e2ca28
Angel <angel.mascarell.tf@gmail.com>
2024-10-09 13:51:09 +0200

    Pagina de incio

---
+++ collectorhubAppWpf/ViewModel/InicioViewModel.cs
@@ -0,0 +1,22 @@
+﻿using collectorhubAppWpf.Model;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows;
+using System.Text;
+using System.IO;
+using Intermodular2DAMGrupoCInterfaces.Models;
+using MVVM.Commands;
+using collectorhubAppWpf.ViewModels;
+
+namespace Intermodular2DAMGrupoCInterfaces.ViewModels
+{
+    public class InicioViewModel : ViewModelBase
+    {
+        private readonly HttpClient _httpClient;
+
+    }
+}
