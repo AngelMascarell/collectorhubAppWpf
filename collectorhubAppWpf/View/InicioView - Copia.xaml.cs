c431a6fb4ae73fa7492d936ad9b94b14942626b3
Angel <angel.mascarell.tf@gmail.com>
2024-10-10 14:17:14 +0200

    login View

---
+++ collectorhubAppWpf/View/InicioView - Copia.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Intermodular2DAMGrupoCInterfaces.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace collectorhubAppWpf.View
+{
+    /// <summary>
+    /// Lógica de interacción para InicioView.xaml
+    /// </summary>
+    public partial class InicioView : Window
+    {
+        public InicioView()
+        {
+            InitializeComponent();
+
+            SearchTextBox.Text = "Buscar manga...";
+            SearchTextBox.Foreground = new SolidColorBrush(Colors.Gray);
+        }
+
+        private void SearchTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchTextBox.Text == "Buscar manga...")
+            {
+                SearchTextBox.Text = string.Empty; 
+                SearchTextBox.Foreground = new SolidColorBrush(Colors.Black); 
+            }
+        }
+
+        private void SearchTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(SearchTextBox.Text))
+            {
+                SearchTextBox.Text = "Buscar manga..."; 
+                SearchTextBox.Foreground = new SolidColorBrush(Colors.Gray); 
+            }
+        }
+
+
+    }
+}
